2659bd4cda8b0065fbd75b373795caf6199c2d43
Dabi <keitadk9@gmail.com>
2024-08-03 20:55:27 +0200

    feat: getTreasureQuestFromString_withValidInput_shouldSucceed pass

--- src/main/java/com/app/treasurequest/TreasureQuestParser.java
+++ src/main/java/com/app/treasurequest/TreasureQuestParser.java
@@ -76,7 +76,61 @@ public class TreasureQuestParser {
 
 
     public static TreasureQuest getTreasureQuestFromString(String content){
-        return null;
+
+        String[] lines = content.split("\n");
+        // process first line
+        String dimensionLine = lines[0];
+        String[] parts = dimensionLine.split(" - ");
+        if(!parts[0].equals(TreasureQuestParser.DIMENSION_KEY)){
+            throw new InvalidIdentifierException(parts[0].charAt(0));
+        }
+        int width = Integer.parseInt(parts[1]);
+        int height = Integer.parseInt(parts[2]);
+        if(width < 0 || height < 0){
+            throw new BoardParserNegativeDimensionException();
+        }
+        Board board = new Board(width,height);
+        List<Adventurer> adventurers = new ArrayList<Adventurer>();
+        AtomicInteger lineIndex =new AtomicInteger(2);
+        Arrays.stream(lines)
+                .skip(1)
+                .forEach(line -> {
+                    String[] chunks = line.split(" - ");
+                    char itemType = chunks[0].charAt(0);
+
+                    if (CellType.MOUNTAIN.getKey() == itemType) {
+                        checkChunksLength(chunks, 3,lineIndex.get());
+                        int mountainJ = Integer.parseInt(chunks[1]);
+                        int mountainI = Integer.parseInt(chunks[2]);
+                        board.createMountain(mountainI, mountainJ);
+                    }
+                    else if (CellType.TREASURE.getKey() == itemType) {
+                        checkChunksLength(chunks, 4,lineIndex.get());
+                        int treasureJ = Integer.parseInt(chunks[1]);
+                        int treasureI = Integer.parseInt(chunks[2]);
+                        int treasureAmount = Integer.parseInt(chunks[3]);
+                        board.createTreasures(treasureAmount, treasureI, treasureJ);
+
+                    }
+                    else if (CellType.ADVENTURER.getKey() == itemType){
+                        checkChunksLength(chunks, 6,lineIndex.get());
+                        String name = chunks[1];
+                        int adventuredJ = Integer.parseInt(chunks[2]);
+                        int adventuredI = Integer.parseInt(chunks[3]);
+                        Orientation orientation = Orientation.valueOf(chunks[4]);
+                        AdventurerSequence[] sequence = Arrays
+                                .stream(chunks[5].split(""))
+                                .map(AdventurerSequence::valueOf)
+                                .toArray(AdventurerSequence[]::new);
+                        adventurers.add(new Adventurer(name,adventuredI, adventuredJ, orientation, sequence));
+
+                    }else{
+                        throw new InvalidIdentifierException(itemType);
+                    }
+                    lineIndex.incrementAndGet();
+                });
+        return new TreasureQuest(board,adventurers);
+
     }
 
     private static void checkChunksLength(String[] chunks, int expectedLength, int line) {
