053145227e26788892180772ac31d951fec09d70
Dabi <keitadk9@gmail.com>
2024-08-04 21:15:06 +0200

    feat: Main added

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -1,8 +1,24 @@
 package main.java.com.app;
 
+import main.java.com.app.treasurequest.TreasureQuest;
+import main.java.com.app.treasurequest.TreasureQuestParser;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hellow word");
+        String input =
+                        "C - 3 - 4\n" +
+                        "M - 1 - 0\n" +
+                        "M - 2 - 1\n" +
+                        "T - 0 - 3 - 2\n" +
+                        "T - 1 - 3 - 3\n" +
+                        "A - Lara - 1 - 1 - S - AADADAGGA";
+
+        TreasureQuest treasureQuest = TreasureQuestParser.getTreasureQuestFromString(input);
+        treasureQuest.runSimulation();
+
+        String output = treasureQuest.toString();
+        System.out.println(output);
+
     }
 }
